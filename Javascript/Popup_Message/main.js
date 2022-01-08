2514fc82f3143f8fed8ec65093209824d16b7da0
AyushBhandariNITK <ayushbhanandari8909@gmail.com>
2022-01-08 12:57:20 +0530

    Popup Message

---
+++ Javascript/Popup_Message/main.js
@@ -0,0 +1,25 @@
+let popupBtn=document.getElementsByClassName('popup');
+let msg=document.querySelector('.msg-box');
+let closeBtn=document.querySelector('.close-btn');
+let overlay=document.getElementById('overlay');
+
+popupBtn.addEventListener('click',function(){
+    openmsg(msg);
+});
+
+closeBtn.addEventListener('click',function(){
+    closemsg(msg);
+});
+
+function openmsg(msg){
+    if(msg==null)   return ;
+    msg.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closemsg(msg){
+    if(msg==null)   return ;
+    msg.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
